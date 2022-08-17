bf573a3629d95eb861ba94efd3a78731788a5e6b
jonathan23c <108855968+jonathan23c@users.noreply.github.com>
2022-08-17 12:52:06 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+def check_answer(guess, a, b):
+    bool = True
+    if a == b:
+    else:
+        print()
+
+bool = True
+my_string = str(bool)
+print(my_string)
+print(type(my_string))
+
+df = pd.DataFrame({'my_timestamp': pd.date_range('2016-1-1 15:00', periods=5)})
+
+>>> df
+         my_timestamp
+2020-05-01 15:00:00
+2021-19-02 15:00:00
+2022-25-03 15:00:00
+2019-31-04 15:00:00
+2024-07-05 15:00:00
